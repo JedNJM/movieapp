1e9374bc1e17ae20b78f85f12207dea493149510
MedJedNJM <81960158+JedNJM@users.noreply.github.com>
2023-02-27 11:29:49 +0100

    Movieupdate

--- src/Components/Desc.jsx
+++ src/Components/Desc.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-import { Card } from 'react-bootstrap/Card';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
 
 const Desc = () => {
-    // useLocation 
-const location = useLocation()
-const movie=location.state.movie
+  // useLocation
+  const location = useLocation();
+  const movie = location.state.movie;
   return (
-    
-    <div>
-    <h1>MOVIE</h1>
-      <h1>{movie.title}</h1>
-      {/* image */}
-       <img src={movie.poster} height={400} alt="error"></img>
-     
-      <h1>{movie.description}</h1>
-</div>
-  )
-}
+    <>
+      <Card className="text-center container-cards">
+        
+        <Card.Body>
+        <Card.Header>{movie.title}</Card.Header>
+        <Card.Text>{movie.description}</Card.Text>
+          <Card.Title>
+            <img src={movie.poster} height={800} alt="error"></img>
+          </Card.Title>
+          
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
-export default Desc
+export default Desc;
